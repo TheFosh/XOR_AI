057d67491d4973005140893da12065830088dd51
Jake Swanson <jakeswan213@gmail.com>
2024-07-01 14:50:13 -0500

    Import all classes

---
+++ neuralNetwork/dense.py
@@ -0,0 +1,21 @@
+from layer import Layer
+import numpy as np
+
+
+class Dense(Layer):
+
+    def __int__(self, input_size, output_size):
+        self.weights = np.random.randn(output_size,input_size) # Random matrix of weights
+        self.bias = np.random.randn(output_size, 1) # Random vector of biases
+
+    # Takes in input vector and produces output vector
+    def forward(self, input):
+        self.input = input
+        return np.dot(self.weights, self.input) + self.bias
+
+    def backward(self, output_gradient, learning_rate):
+        weights_gradient = np.dot(output_gradient, self.input.T)
+        self.weights -= learning_rate * weights_gradient
+        self.bias -= learning_rate * output_gradient
+        return np.dot(self.weights.T, output_gradient)
+        pass
