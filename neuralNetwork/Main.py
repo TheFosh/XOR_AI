fe915e625efe865b86053cac41c910832a2511e7
Jake Swanson <jakeswan213@gmail.com>
2024-07-03 11:06:28 -0500

    Made function for plain
    - Creates plain of points in 2D space
    - Gives each point into the network
    - Records all outputs and puts them in a file

--- neuralNetwork/Main.py
+++ neuralNetwork/Main.py
@@ -3,11 +3,31 @@ from tanh import Tanh
 from mse import mse, mse_prime
 import numpy as np
 
+def CreatePlainPredictions(_network):
+    # [0, 0.1, ... , 1](1 x 11) + [0, 0.1, ... , 1](11 x 1)
+    X = []
+    for rowInd in range(11):
+        row = []
+        for colInd in range(11):
+            row.append([colInd/10, rowInd/10])
+        X.append(row)
+    X = np.array(X).reshape((121,2,1))
+
+    count = 1
+    outputList = []
+    for x in zip(X):
+        # Forward
+        output = x
+        for layer in _network:
+            output = layer.forward(output)
+        outputList.append(round(np.sum(output)), 4)
+        count += 1
+    pass
+
 #
 X = np.reshape([[0,0],[0,1],[1,0],[1,1]],(4,2,1))
 Y = np.reshape([[0],[1],[1],[0]], (4,1,1))
 #
-
 network = [
     Dense(2,3),
     Tanh(),
@@ -15,7 +35,7 @@ network = [
     Tanh()
 ]
 #
-cycles = 100000
+cycles = 10000
 learning_rate = 0.1
 
 # train
@@ -37,3 +57,7 @@ for e in range(cycles):
 
     error /= len(x)
     print("%d/%d, error = %f" % (e + 1, cycles, error))
+
+# Creating 3D graph
+# [0, 0.1, ... , 1](1 x 11) + [0, 0.1, ... , 1](11 x 1)
+CreatePlainPredictions(network)
