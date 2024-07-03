6a7df98aee5ba8175c18cba5ae5141006a460726
Jake Swanson <jakeswan213@gmail.com>
2024-07-03 13:37:53 -0500

    Fixed output string
    Out put had unnecessary brackets

--- neuralNetwork/Main.py
+++ neuralNetwork/Main.py
@@ -20,7 +20,7 @@ def CreatePlainPredictions(_network):
         output = x
         for layer in _network:
             output = layer.forward(output)
-        outputList.append(str(round(np.sum(output),4)) +" "+ str(x[0][0]) +" "+ str(x[0][1]) +"\n")
+        outputList.append(str(round(np.sum(output),4)) +" "+ str(x[0][0][0]) +" "+ str(x[0][1][0]) +"\n")
         count += 1
 
         createFile(outputList)
